26887b22a627b9d1619610b2c108a153ec4078a2
SarraSbaa <57116007+SarraSb97@users.noreply.github.com>
2023-01-08 23:05:19 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -6,7 +6,7 @@
 
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # SarraSb97
 
    # Input:
    # p, principal amount
@@ -26,5 +26,3 @@
    s=`expr $p \* $t \* $r / 100`
    echo "The simple interest is: "
    echo $s
-
-read s
